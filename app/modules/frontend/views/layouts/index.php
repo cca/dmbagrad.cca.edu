8152ae92a393c28d7cf8a92cc3dff0127f140d5b
phette23 <phette23@gmail.com>
2016-07-07 16:58:05 -0700

    add google analytics tracking code

--- app/modules/frontend/views/layouts/index.php
+++ app/modules/frontend/views/layouts/index.php
@@ -70,5 +70,15 @@
 
 	<input type='hidden' name='query_inp'>
 	<h6 style='text-align: center;'>© 2016 California College of the Arts, unless otherwise noted</h6>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-131604-20', 'auto');
+ga('send', 'pageview');
+</script>
 	</body>
 </html>
